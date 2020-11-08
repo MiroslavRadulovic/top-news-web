07b94871fe8392f4dab82b25c23769def541568f
MiroslavRadulovic <miroslav.radulovic92@gmail.com>
2020-11-08 10:11:55 +0100

    firx categories style on mobile resolution

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { categoryList } from './categoryList';
 
 import Category from './Category/Category';
-import './Categories.scss';
 
 const Categories = () => {
   return (
-    <div className="categories">
+    <>
       {categoryList.map((category) => (
         <React.Fragment key={category}>
           <Category categoryName={category} />
         </React.Fragment>
       ))}
-    </div>
+    </>
   );
 };
 
